a72a5aab6228988a37eef0b59868f4b04c7d0d3c
prabushi <prabushisamarakoon@gmail.com>
2019-07-10 19:05:39 +0530

    Update user registration system artifacts

---
+++ UserRegistrationSystem/UserRegistrationSystemClassMediatorProject/src/main/java/org/wso2/demo/encryptor/UserRegistrationEncryptorMediator.java
@@ -0,0 +1,13 @@
+package org.wso2.demo.encryptor;
+
+import org.apache.synapse.MessageContext; 
+import org.apache.synapse.mediators.AbstractMediator;
+
+public class UserRegistrationEncryptorMediator extends AbstractMediator { 
+
+	public boolean mediate(MessageContext context) { 
+		// TODO Implement your mediation logic here 
+		
+		return true;
+	}
+}
